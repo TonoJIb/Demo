8133c5dda42649cc192874e2fa4421bddf68a8b5
TonoJIb <mobil1234@inbox.ru>
2015-10-06 09:08:05 +0300

    Alt Im

--- javascripts/Demo1_script.js
+++ javascripts/Demo1_script.js
@@ -66,8 +66,8 @@ $( document ).ready(function() {
 	im.src="/images/Lympho.jpg";
 	 im.alt="Image not found" 
 	 im.onError = function() {
-	 	this.onerror=null;
-	 	this.src='/Demo/images/Lympho.jpg';
+	 	im.onerror=null;
+	 	im.src='/Demo/images/Lympho.jpg';
 	 } 
 	im.onload=function(){ctx.drawImage(im,0,0)
 		var imgData = ctx.getImageData(0, 0, c.width, c.height);
