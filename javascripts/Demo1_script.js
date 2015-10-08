d86b07d80fc3eb4d15cbd20e636e29df0b6cc79f
TonoJIb <mobil1234@inbox.ru>
2015-10-08 13:28:15 +0300

    cluster tree added

--- javascripts/Demo1_script.js
+++ javascripts/Demo1_script.js
@@ -1,9 +1,13 @@
 function argarrmin(arr){
 	var x=0;
-	var y=1;
-	var minimum=arr[x][y];
+	var y=0;
+	
+	var minimum=1000;
+
 	for (var i=0; i<arr.length; i++){
+		if (arr[i]===undefined) continue;
 		for (var j=0; (i!=j)&&(j<i); j++){
+			if (arr[i][j]===undefined) continue;
 			if (arr[i][j]<minimum){
 				x=i;
 				y=j;
@@ -11,7 +15,7 @@ function argarrmin(arr){
 			}
 		}
 	}
-	return [x, y, minimum];
+	return {"x":x, "y":y, "mindist":minimum};
 }
 function distance(point1, point2)
 {
@@ -75,7 +79,7 @@ $( document ).ready(function() {
 		var imgData = ctx.getImageData(0, 0, c.width, c.height);
     	// invert colors
     	var i;
-    	var k = 30;
+    	var k = 5;
     	var j;
     	var clusters=[];// = [[0, 0, 0],[0, 0, 0],[0,0,0]];
     	var div = document.getElementById("axis");
@@ -160,15 +164,214 @@ var color3=rgbToHex(clusters[2]);
 				distmatrix[j][i]=distmatrix[i][j];
 			}
 		}
-		console.log(argarrmin(distmatrix));
+
+		//console.log(argarrmin(distmatrix));
 	//Form a table of agglomeration
 		var aggtable=[];
-		for (i=0; i<k; i++){
+		var aggobject={"distance":0,"clusters":clusters};
+		aggtable.push(aggobject);
+		var step = argarrmin(distmatrix);
+		var clustersind={}; 
+		var cx, cy, cxy, newD; //temp variables
+		var distances=[0]; //agglomeration distances this will increase on each step by new agglomeration distance
+		var initdistmatrix=distmatrix; //make a copy of distmatrix
+		var newline=[];
+		var alpha=0.5; //algorithm parameters
+		var beta=0;
+		var gamma=0.5;
+		var undef=newline[1];
+		var tree=[]; for (i=0;i<k;i++){tree[i]={"nodeid": i,"distance":0,"children":[]}};
+		for (i=0; i<k-1; i++){
+			cxy=[step.x, step.y];
+			clusterind = {"nodeid":tree.length, "distance":step.mindist,"children":cxy,"parent":null};
+			distances.push(step.mindist);
+			tree[step.x].parent = tree.length;
+			tree[step.y].parent = tree.length;
+			tree.push(clusterind);
+			
+			//distmatrix.correction
+			newline=[];
+			dxy = distmatrix[step.x][step.y];
+			delete distmatrix[step.x];
+			delete distmatrix[step.y];
+			for (j=0; j<distmatrix.length;j++){
+				if (distmatrix[j]===undefined) 
+				{	newline.push(undef);
+					continue;}
+				newD=alpha*(distmatrix[j][step.x]+distmatrix[j][step.y])+beta*dxy+gamma*Math.abs(distmatrix[j][step.x]-distmatrix[j][step.y]);
+				distmatrix[j].push(newD);
+				newline.push(newD);
+				distmatrix[j][step.x]=undef;
+				distmatrix[j][step.y]=undef;
+			}
+			newline.push(0);
+			distmatrix.push(newline);
 
-			//var aggobject = {iter: i,}
+			step = argarrmin(distmatrix);
+
 		}
+		// forming json
+		var jsontree;
+		var obj;
+		var jsonstring= function(node){
+			if (tree[node].children[1]===undefined) {
+				obj={
+					name: tree[node].nodeid,
+					parent: tree[node].parent
+				}
+				return obj;
+			} else
+			{
+				obj={
+					name: tree[node].nodeid,
+					parent: tree[node].parent,
+					children: [jsonstring(tree[node].children[0]),jsonstring(tree[node].children[1])]
+				}
+				//var str="{'name':"+ tree[node].nodeid +", 'parent':"+tree[node].parent+", 'children': ["+jsonstring(tree[node].children[0])+", "+jsonstring(tree[node].children[1])+"]"
+			return obj};
+		}
+		jsontree=jsonstring(tree.length-1);
 
-	};
+		// a tree
+		var margin = {top: 20, right: 120, bottom: 20, left: 120},
+    		width = 960 - margin.right - margin.left,
+    		height = 800 - margin.top - margin.bottom;
+
+		var i = 0,
+    		duration = 750,
+    		root;
+
+		var tree = d3.layout.tree()
+    				 .size([height, width]);
+
+		var diagonal = d3.svg.diagonal()
+    		.projection(function(d) { return [d.y, d.x]; });
+
+		var svg = d3.select("#axis")
+								   .append("svg")
+    							   .attr("width", width + margin.right + margin.left)
+    							   .attr("height", height + margin.top + margin.bottom)
+  								   .append("g")
+    							   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  		root = jsontree;
+  		root.x0 = height / 2;
+  		root.y0 = 0;
+
+  		function collapse(d) {
+    		if (d.children) {
+      			d._children = d.children;
+      			d._children.forEach(collapse);
+      			d.children = null;
+    		}
+  		}
+
+  		root.children.forEach(collapse);
+  		update(root);
+
+ 		d3.select(self.frameElement).style("height", "800px");
+
+function update(source) {
+
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse(),
+      links = tree.links(nodes);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+  // Update the nodes…
+  var node = svg.selectAll("g.node")
+      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  // Enter any new nodes at the parent's previous position.
+  var nodeEnter = node.enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+      .on("click", click);
+
+  nodeEnter.append("circle")
+      .attr("r", 1e-6)
+      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeEnter.append("text")
+      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+      .text(function(d) { return d.name; })
+      .style("fill-opacity", 1e-6);
+
+  // Transition nodes to their new position.
+  var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+  nodeUpdate.select("circle")
+      .attr("r", 4.5)
+      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeUpdate.select("text")
+      .style("fill-opacity", 1);
+
+  // Transition exiting nodes to the parent's new position.
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+      .remove();
+
+  nodeExit.select("circle")
+      .attr("r", 1e-6);
+
+  nodeExit.select("text")
+      .style("fill-opacity", 1e-6);
+
+  // Update the links…
+  var link = svg.selectAll("path.link")
+      .data(links, function(d) { return d.target.id; });
+
+  // Enter any new links at the parent's previous position.
+  link.enter().insert("path", "g")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        var o = {x: source.x0, y: source.y0};
+        return diagonal({source: o, target: o});
+      });
+
+  // Transition links to their new position.
+  link.transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+  // Transition exiting nodes to the parent's new position.
+  link.exit().transition()
+      .duration(duration)
+      .attr("d", function(d) {
+        var o = {x: source.x, y: source.y};
+        return diagonal({source: o, target: o});
+      })
+      .remove();
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(d) {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+}
+
+// Toggle children on click.
+function click(d) {
+  if (d.children) {
+    d._children = d.children;
+    d.children = null;
+  } else {
+    d.children = d._children;
+    d._children = null;
+  }
+  update(d);
+}
+
+	}//end onload;
 	
 
-})
+})//end ondocumentready
